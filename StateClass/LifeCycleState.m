2a5286c42568c384bb052e26e2ef25e1f117e8d5
Silvertraces <hao1985563301@gmail.com>
2025-06-10 17:40:50 +0800

    feat(LifeCycleState): 添加根据实例数组生成分类数组的方法
    新增 toCategoricalFromInstance 方法，可根据枚举实例数组动态生成分类数组，仅包含输入数组中存在的状态作为分类类别，并保持枚举定义的顺序。相比静态方法 toCategorical 更加灵活，适用于处理部分枚举状态的情况。

--- StateClass/LifeCycleState.m
+++ StateClass/LifeCycleState.m
@@ -31,4 +31,33 @@ classdef LifeCycleState < uint8
             catArray = categorical(enumNames, allCategories, 'Ordinal', true, 'Protected', true);
         end
     end
+
+    methods
+        function catArray = toCategoricalFromInstance(objArray)
+            % toCategoricalFromInstance 根据当前 LifeCycleState 实例数组创建 categorical 数组
+            %
+            %   catArray = objArray.toCategoricalFromInstance()
+            %
+            %   该方法接受一个 LifeCycleState 枚举数组作为输入，并返回一个
+            %   categorical 数组。与静态方法 toCategorical 不同，此方法
+            %   仅使用输入数组中存在的枚举状态作为 categorical 数组的类别。
+            %   生成的 categorical 数组将是有序的，并保留 LifeCycleState
+            %   枚举中定义的顺序。
+
+            % 提取枚举成员名称
+            enumNames = arrayfun(@(x) string(x), objArray);
+            
+            % 获取输入数组中存在的唯一类别，并保持 LifeCycleState 定义的顺序
+            % 先转换为双精度数组，然后排序，再转换回 LifeCycleState 枚举，最后提取唯一值
+            uniqueEnumValues = unique(objArray);
+            [~, sortedIdx] = sort(uniqueEnumValues);
+            sortedUniqueEnumValues = uniqueEnumValues(sortedIdx);
+            
+            % 将数值转换回 LifeCycleState 枚举，然后转换为字符串
+            instanceCategories = arrayfun(@(x) string(LifeCycleState(x)), sortedUniqueEnumValues);
+            
+            % 生成有序 categorical 数组
+            catArray = categorical(enumNames, instanceCategories, 'Ordinal', true, 'Protected', true);
+        end
+    end
 end
